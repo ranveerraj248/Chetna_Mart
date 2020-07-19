5f3546c1408c8d996f9b39adee54f934219d61ba
Mishra07adi <mishra07adi@gmail.com>
2020-07-20 00:33:19 +0530

    Sign Up page Complete

--- app/src/main/java/com/example/chetnamart/RegisterActivity.java
+++ app/src/main/java/com/example/chetnamart/RegisterActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class RegisterActivity extends AppCompatActivity {
+
+    //private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //frameLayout = findViewById(R.id.register_framelayout);
+        //setFragment();
     }
+
+    //private void setFragment()  {}
+
+
 }
