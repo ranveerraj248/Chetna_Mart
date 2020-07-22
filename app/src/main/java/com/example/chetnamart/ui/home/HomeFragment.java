0bc3087ea5017c58b73f8581a1186015e6a81f91
ranveerraj248 <ranveerraj248@gmail.com>
2020-07-22 19:30:09 +0530

    pager2

--- app/src/main/java/com/example/chetnamart/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/chetnamart/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.chetnamart.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,25 +14,127 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.chetnamart.R;
+import com.example.chetnamart.SliderAdapter;
+import com.example.chetnamart.SliderModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    public HomeFragment(){
+        //Required empty public constructor
+    }
+    private RecyclerView categoryRecyclerView;
+    private CategoryAdapter categoryAdapter;
+
+    ////////// Banner Slider
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+    private int currentPage = 2;
+    private Timer timer;
+    final private long DELAY_TIME = 3000;
+    final private long PERIOD_TIME = 3000;
+    /////////Banner Slider
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+
+        ////////// Banner Slider
+        bannerSliderViewPager = view.findViewById(R.id.vpBannerSlider);
+        sliderModelList = new ArrayList<SliderModel>();
+
+        sliderModelList.add(new SliderModel(R.drawable.app_icon));
+        sliderModelList.add(new SliderModel(R.drawable.ic_menu_camera));
+
+        sliderModelList.add(new SliderModel(R.drawable.logo));
+        sliderModelList.add(new SliderModel(R.drawable.items_banner));
+        sliderModelList.add(new SliderModel(R.drawable.app_icon));
+        sliderModelList.add(new SliderModel(R.drawable.ic_menu_camera));
+
+        sliderModelList.add(new SliderModel(R.drawable.logo));
+        sliderModelList.add(new SliderModel(R.drawable.items_banner));
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if(state==ViewPager.SCROLL_STATE_IDLE){
+                    pageLooper();
+                }
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+
+        startBannerSlideShow();
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                pageLooper();
+                stopBannerSlideShow();
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    startBannerSlideShow();
+                }
+                return false;
             }
         });
+        ////////// Banner Slider
         return root;
     }
+    ////////// Banner Slider
+    private void pageLooper(){
+      if(currentPage == sliderModelList.size()-2){
+          currentPage = 2;
+          bannerSliderViewPager.setCurrentItem(currentPage,false);
+      }
+        if(currentPage == 1){
+            currentPage = sliderModelList.size() -3;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+    }
+
+    private void startBannerSlideShow(){
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if(currentPage >=sliderModelList.size()){
+                    currentPage = 1;
+                }
+               bannerSliderViewPager.setCurrentItem(currentPage++,true);
+            }
+        };
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },DELAY_TIME,PERIOD_TIME);
+    }
+    private void stopBannerSlideShow(){
+        timer.cancel();
+    }
+    ////////// Banner Slider
 }
